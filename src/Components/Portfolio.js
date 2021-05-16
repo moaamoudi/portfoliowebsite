b37a51c23cfe59a5a80423a4849b576ac8ecdab8
mohamoudi <78143032+moaamoudi@users.noreply.github.com>
2021-05-16 08:52:56 +0300

    removed buttons

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 
 export default function Portfolio() {
-  const history = useHistory();
   return (
     <div className="App">
       <header className="App-header">
         <h1>PORTFOLIO</h1>
-        <button onClick={() => history.push("/")}>home</button>
       </header>
     </div>
   );
--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 
 export default function Work() {
-  const history = useHistory();
   return (
     <div className="App">
       <header className="App-header">
         <h1>Work in Progress...</h1>
-        <button onClick={() => history.push("/admin")}>admin</button>
       </header>
     </div>
   );
